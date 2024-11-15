3f03c051c6ad7d4b851e5b1630b08db190ad44ad
Yihuai Gao <yihuai@stanford.edu>
2024-11-15 09:56:18 +0800

    Update bimanual examples

--- python/examples/test_bimanual.py
+++ python/examples/test_bimanual.py
@@ -7,7 +7,7 @@ ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(ROOT_DIR)
 os.chdir(ROOT_DIR)
 import arx5_interface as arx5
-
+import click
 
 def easeInOutQuad(t):
     t *= 2
@@ -17,11 +17,16 @@ def easeInOutQuad(t):
         t -= 1
         return -(t * (t - 2) - 1) / 2
 
-
-def main():
+@click.command()
+@click.argument("model0")  # ARX arm model: X5 or L5
+@click.argument("interface0")  # can bus name (can0 etc.)
+@click.argument("model1")  # ARX arm model: X5 or L5
+@click.argument("interface1")  # can bus name (can0 etc.)
+def main(model0: str, interface0: str, model1: str, interface1: str):
     np.set_printoptions(precision=3, suppress=True)
-    arx5_0 = arx5.Arx5JointController("X5", "can0")
-    arx5_1 = arx5.Arx5JointController("X5", "can1")
+    assert(interface0 != interface1)
+    arx5_0 = arx5.Arx5JointController(model0, interface0)
+    arx5_1 = arx5.Arx5JointController(model1, interface1)
     robot_config = arx5_0.get_robot_config()
     controller_config = arx5_0.get_controller_config()
 
@@ -38,12 +43,12 @@ def main():
         cmd.gripper_pos = easeInOutQuad((i / (step_num - 1))) * 0.08
         arx5_0.set_joint_cmd(cmd)
         arx5_1.set_joint_cmd(cmd)
-        JointState = arx5_0.get_joint_state()
-        JointState = arx5_1.get_joint_state()
-        arm_dof_pos = JointState.pos().copy()
-        arm_dof_vel = JointState.vel().copy()
+        joint_state = arx5_0.get_joint_state()
+        joint_state = arx5_1.get_joint_state()
+        arm_dof_pos = joint_state.pos().copy()
+        arm_dof_vel = joint_state.vel().copy()
         # print(arm_dof_pos, arm_dof_vel)
-        # print(f"gripper: {JointState.gripper_pos:.05f}")
+        # print(f"gripper: {joint_state.gripper_pos:.05f}")
         time.sleep(controller_config.controller_dt)
 
     for i in range(step_num):
@@ -55,9 +60,9 @@ def main():
         arx5_0.set_joint_cmd(cmd)
         arx5_1.set_joint_cmd(cmd)
         time.sleep(controller_config.controller_dt)
-        JointState = arx5_0.get_joint_state()
-        JointState = arx5_1.get_joint_state()
-        # print(f"gripper: {JointState.gripper_pos:.05f}")
+        joint_state = arx5_0.get_joint_state()
+        joint_state = arx5_1.get_joint_state()
+        # print(f"gripper: {joint_state.gripper_pos:.05f}")
 
 
 main()
