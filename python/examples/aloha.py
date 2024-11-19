cc684e1e6148a8a616240694c48ab369ebcf88bf
Yihuai Gao <yihuai@stanford.edu>
2024-11-20 07:33:01 +0800

    Attempt to add EC motor querying

--- python/examples/aloha.py
+++ python/examples/aloha.py
@@ -24,6 +24,7 @@ def main(master_model: str, master_interface: str, slave_model: str, slave_inter
     master = arx5.Arx5JointController(master_model, master_interface)
     slave = arx5.Arx5JointController(slave_model, slave_interface)
     robot_config = master.get_robot_config()
+    assert robot_config.joint_dof == slave.get_robot_config().joint_dof
     controller_config = master.get_controller_config()
 
     master.reset_to_home()
@@ -34,14 +35,17 @@ def main(master_model: str, master_interface: str, slave_model: str, slave_inter
     master.set_gain(gain)
     try:
         while True:
-            joint_state = master.get_joint_state()
-            joint_state.timestamp = 0
-            joint_state.torque()[:] = 0.0
-            joint_state.vel()[:] *= 0.3  # add some speed for lower latency. 
-            # You can set it to 0.0 if you only want to use position control
-            joint_state.gripper_torque = 0.0
-            joint_state.gripper_vel = 0.0
-            slave.set_joint_cmd(joint_state)
+            master_joint_state = master.get_joint_state()
+            slave_joint_cmd = arx5.JointState(robot_config.joint_dof)
+            slave_joint_cmd.pos()[:] = master_joint_state.pos()
+            slave_joint_cmd.gripper_pos = master_joint_state.gripper_pos
+            
+            # If you want to decrease the delay of teleoperation , you can uncomment the following line
+            # This will partially include the velocity to the command, and you will need to pass these velocities
+            # into the policy layout
+            # slave_joint_cmd.vel()[:] = master_joint_state.vel()* 0.3 
+            
+            slave.set_joint_cmd(slave_joint_cmd)
             time.sleep(controller_config.controller_dt)
     except KeyboardInterrupt:
         print("\nKeyboard interrupt detected. Resetting arms to home position...")
